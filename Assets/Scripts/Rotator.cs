1e42d5836ced27e6d38919413bf53f5a29f009c0
GRGRV9 <niamod@mail.ru>
2021-04-02 00:50:24 +0900

    Update Rotator
    Update Rotator

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    float T;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,7 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        T = Mathf.Sin(Time.time * 2f) * 0.5f;
+        transform.Rotate(new Vector3(0, 1, 0), T);
     }
 }
